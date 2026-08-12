a84e16f001d3e156da87af57e4ed72f9d4308d02
tuyenle <tuyendle92@gmail.com>
2026-08-12 18:06:04 +0700

    remove unused import

--- src/components/appbar.jsx
+++ src/components/appbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useId, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeTheme } from '../slices/game-slice.js';
 import { useNavigate } from "react-router-dom";
